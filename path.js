f02a25e8b0143a448a1fc0ae7cccd1a6130ea7c2
elishakarki <karkielisha222@gmail.com>
2024-02-15 18:57:07 +0545

    sending json files

---
+++ path.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+// 👇️ if you use CommonJS
+// const express = require('express')
+
+const app = express();
+// this line of code executes on localhost:5000
+app.get('/', (req, res) => {
+  res.send('bobbyhadz.com');
+});
+// this line of code executes on localhost:500/books
+app.get('/books', (req, res) => {
+  res.status(200).send([
+    {id: 1, title: 'book 1'},
+    {id: 2, title: 'book 2'},
+  ]);
+});
+// this line of code executes  when the  path that is not mentioned above is typed in url
+app.get('*', (req, res) => {
+    res.send('404 Page Not Found');
+  });
+const port = 5000;
+// if the listen of server is not written the server is not run and it does not listen to the changes
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
